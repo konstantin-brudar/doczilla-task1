d87d5bc3cef20c22d38667a8cf37fe9345814a70
Konstantin Brudar <brudar@bk.ru>
2024-08-24 23:23:06 +0300

    Get all files in directory recursively

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("""
+            Usage: java Main <path>
+            <path> - root directory with text files
+            """);
+            System.exit(0);
+        }
+
+        final String ROOT_DIR = args[0];
+        try {
+            List<String> fileList = getAllFiles(ROOT_DIR);
+            fileList.forEach(System.out::println);
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+
+    private static List<String> getAllFiles(String rootDir) throws IOException {
+        List<String> fileList = new ArrayList<>();
+        Files.walk(Paths.get(rootDir))
+             .map(Path::toString)
+             .forEach(fileList::add);
+        return fileList;
+    }
+}
