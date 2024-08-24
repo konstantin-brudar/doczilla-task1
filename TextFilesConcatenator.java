d97dd1de1b4771548c7245f8a967cb33b0a8461e
Konstantin Brudar <brudar@bk.ru>
2024-08-25 00:05:17 +0300

    Concatenate all files to one output file

---
+++ TextFilesConcatenator.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+public class TextFilesConcatenator {
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("""
+            Program concatenates all text files in directory to one output file
+            Usage: java TextFilesConcatenator <root-dir> <out-file>
+            <root-dir> - root directory containing text files
+            <out-file> - path to output file
+            """);
+            System.exit(0);
+        }
+
+        final String ROOT_DIR = args[0];
+        final String OUTPUT_FILE = args[1];
+        try {
+            List<String> fileList = getAllFiles(ROOT_DIR);
+            concatenateFiles(fileList, OUTPUT_FILE);
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+
+    private static List<String> getAllFiles(String rootDir) throws IOException {
+        List<String> fileList = new ArrayList<>();
+        Files.walk(Paths.get(rootDir))
+            .filter(Files::isRegularFile)
+            .filter(path -> path.toString().endsWith(".txt"))
+            .map(Path::toString)
+            .sorted()
+            .forEach(fileList::add);
+        return fileList;
+    }
+
+    private static void concatenateFiles(List<String> fileList, String outputFile) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
+            for (String file : fileList) {
+                try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        writer.write(line);
+                        writer.newLine();
+                    }
+                }
+            }
+        }
+    }
+}
